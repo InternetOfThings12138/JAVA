dd0cb1fe1755eafa9fde56e0719895d1053ec2a9
Wsh <575529237@qq.com>
2021-06-01 12:24:57 +0800

    addLeetCode

---
+++ LeetCodeJAVA/src/Solution49.java
@@ -0,0 +1,14 @@
+public class Solution49 {
+    public List<List<String>> groupAnagrams(Stirng[] strs){
+        Map<String,List<String>> map = new HashMap<String,List<String>>();
+        for(String str:strs){
+            char[] array = str.toCharArray();
+            Arrays.sort(array);
+            String key = new String(array);
+            List<String> list = map.getOrDefault(key,new ArrayList<String>());
+            list.add(str);
+            map.put(key,list);
+        }
+        return new ArrayList<List<String>>(map.values());
+    }
+}
